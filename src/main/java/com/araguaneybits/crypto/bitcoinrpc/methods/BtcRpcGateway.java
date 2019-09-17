c4e06506d1ce9e2487c98ef6adbf853046512e00
Jose Luis Estevez <jose.estevez.prieto@gmail.com>
2019-09-17 07:17:27 +0200

    Bitcoin rpc gateway using okhttp wrapper

---
+++ src/main/java/com/araguaneybits/crypto/bitcoinrpc/methods/BtcRpcGateway.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2019 AraguaneyBits.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.araguaneybits.crypto.bitcoinrpc.methods;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.araguaneybits.commons.constants.HttpConstants;
+import com.araguaneybits.commons.http.ApiHttpClient;
+import com.araguaneybits.commons.http.ApiHttpMediaType;
+import com.araguaneybits.commons.http.ApiHttpResponse;
+import com.araguaneybits.commons.http.ApiHttpType;
+import com.araguaneybits.commons.http.EnumHttpStatusCode;
+import com.araguaneybits.commons.utils.HttpUtils;
+import com.araguaneybits.commons.utils.ProxyConfiguration;
+import com.araguaneybits.crypto.bitcoinrpc.RpcInputMessage;
+import com.araguaneybits.crypto.bitcoinrpc.exception.AuthenticationException;
+import com.araguaneybits.crypto.bitcoinrpc.exception.CallRpcApiMethodException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.HttpUrl;
+
+/**
+ * The Class BtcRpcGateway.
+ *
+ * @author jestevez
+ */
+public class BtcRpcGateway {
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(BtcRpcGateway.class);
+
+    /** The api http client. */
+    private ApiHttpClient apiHttpClient;
+
+    /** The credential. */
+    private String credential;
+
+    /**
+     * Instantiates a new btc rpc gateway.
+     *
+     * @param httpUrl the http url
+     * @param rpcUser the rpc user
+     * @param rpcPassword the rpc password
+     * @param proxyConfiguration the proxy configuration
+     */
+    public BtcRpcGateway(HttpUrl httpUrl, String rpcUser, String rpcPassword, ProxyConfiguration proxyConfiguration) {
+        apiHttpClient = new ApiHttpClient(httpUrl, proxyConfiguration);
+        credential = HttpUtils.toBasicAuthorization(rpcUser, rpcPassword);
+    }
+
+    /**
+     * Instantiates a new btc rpc gateway.
+     *
+     * @param httpUrl the http url
+     * @param rpcUser the rpc user
+     * @param rpcPassword the rpc password
+     */
+    public BtcRpcGateway(HttpUrl httpUrl, String rpcUser, String rpcPassword) {
+        this(httpUrl, rpcUser, rpcPassword, new ProxyConfiguration());
+    }
+
+    /**
+     * Instantiates a new btc rpc gateway.
+     *
+     * @param rpcUser the rpc user
+     * @param rpcPassword the rpc password
+     * @param rpcHost the rpc host
+     * @param rpcPort the rpc port
+     */
+    public BtcRpcGateway(String rpcUser, String rpcPassword, String rpcHost, String rpcPort) {
+        this(HttpUrl.parse("http://" + rpcHost + ":" + rpcPort + "/"), rpcUser, rpcPassword, new ProxyConfiguration());
+    }
+
+    /**
+     * Builds the rpc param.
+     *
+     * @param callMethod the call method
+     * @param params the params
+     * @return the string
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    private static String buildRpcParam(String callMethod, Object... params) throws IOException {
+        RpcInputMessage body = new RpcInputMessage();
+        body.setMethod(callMethod);
+        if (params != null && params.length > 0) {
+            body.setParams(params);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String data = objectMapper.writeValueAsString(body);
+        LOGGER.debug("RPC Params: [{}]", data);
+        return data;
+    }
+
+    /**
+     * Call rpc api method.
+     *
+     * @param callMethod the call method
+     * @param params the params
+     * @return the string
+     */
+    public String callRpcApiMethod(String callMethod, Object... params) {
+        try {
+            LOGGER.trace("RPC Request: Method: [{}] Params [{}] ", callMethod, params);
+            String data = buildRpcParam(callMethod, params);
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put(HttpConstants.AUTHORIZATION, String.format(credential));
+            ApiHttpResponse apiResponse = apiHttpClient.send(ApiHttpType.POST, headers, data, ApiHttpMediaType.APPLICATION_FORM_URLENCODED);
+            if (apiResponse.getCode() == EnumHttpStatusCode.HTTP_UNAUTHORIZED.getCode()) {
+                throw new AuthenticationException("http unauthorized");
+            }
+            String json = apiResponse.getBody();
+            LOGGER.trace("RPC Response: [{}]", json);
+            return json;
+        } catch (Exception e) {
+            LOGGER.error("Fail in callRpcApiMethod", e);
+            throw new CallRpcApiMethodException("Fail to call method " + callMethod);
+        }
+    }
+
+}
